aefb910760f53e3d1be7cbd65764cfbeeadd4192
Neal Boylan <120754481+neal-boylan@users.noreply.github.com>
2024-01-22 17:18:51 +0000

    upload playtime 0.2.0 files

---
+++ src/server.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+import Hapi from "@hapi/hapi";
+import Vision from "@hapi/vision";
+import Cookie from "@hapi/cookie";
+import Handlebars from "handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+import { webRoutes } from "./web-routes.js";
+import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
+
+async function init() {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+  await server.register(Vision);
+  await server.register(Cookie);
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "./views",
+    layoutPath: "./views/layouts",
+    partialsPath: "./views/partials",
+    layout: true,
+    isCached: false,
+  });
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
+  db.init();
+  server.route(webRoutes);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
